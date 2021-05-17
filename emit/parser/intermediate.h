d9ebaca70e2b7164d039142474ac5516043671cb
EvaCh7 <73312284+EvaCh7@users.noreply.github.com>
2021-05-17 23:52:02 +0300

    offset

--- emit/parser/intermediate.h
+++ emit/parser/intermediate.h
@@ -7,9 +7,13 @@
 #define CURR_SIZE   (total*sizeof(Quad))
 #define NEW_SIZE    (EXPAND_SIZE*sizeof(Quad)+CURR_SIZE)
 
+// label list
+typedef int llist_t;
+
 typedef struct Quad         Quad;
 typedef struct Expression   Expr;
 typedef struct Call         Call;
+typedef struct Stmt         Stmt;
 
 extern Quad*            quads;
 extern unsigned         total;
@@ -57,8 +61,17 @@ struct Expression {
     char* strConst;
     unsigned char boolConst;
 
+    // in case of boolexpr
+    llist_t truelist;
+    llist_t falselist;
+    llist_t test;
+    llist_t enter;
+
+
     struct Expression* next; // next
+    struct Expression* next_index;
 };
+
 
 struct Quad {
     enum iopcode_t op;
@@ -76,21 +89,55 @@ struct Call {
     Expr *elist;
 };
 
+struct Stmt {
+    llist_t breaklist;
+    llist_t contlist;
+};
+
+// VM ASM creation
+unsigned next_quad(void);
+int is_arith(Expr *e);
 void init_quads(void);
 void emit(enum iopcode_t op, Expr* result, Expr* arg1, Expr* arg2, unsigned label, unsigned line);
-void emit_compares(enum iopcode_t op, Expr *result, Expr *arg1, Expr *arg2, SymTable *s, unsigned label, unsigned line);
-void emit_logical(enum iopcode_t op, Expr *result, Expr *arg1, Expr *arg2, SymTable *s, unsigned label, unsigned line);
+int mk_bool_vmasm(Expr *e);
+int mk_nbe_vmasm(enum iopcode_t	op,Expr *e);
+
+// Expression creation
+Expr *expr(enum expression_type_t  type);
 Expr *sym_expr(SymTableEntry *e);
+Expr *string_expr(char *str);
 Expr *nil_expr(void);
 Expr *num_expr(double num);
 Expr *bool_expr(unsigned char bool);
+Expr *member_expr(SymTable *t, int scope, Expr *lvalue, char *name);
+
+//offset 
+scopespace_t currscopespace(void);
+unsigned currscopeoffset(void);
+void inccurrscopeoffset(void);
+void enterscopespace(void);
+void exitscopespace(void);
+void resetformalargsoffset(void);
+void resetfunctionlocalsoffset(void);
+void restorecurrscopeoffset(unsigned n);
+
+
+void reset_temp_counter(void);
 SymTableEntry *new_temp(SymTable *t, int scope);
 Expr *make_call(SymTable *t, int scope, Expr *call, Expr *revelist);
 Expr *reverse_elist(Expr **elist);
+Expr *emit_if_table_item(SymTable *t, int scope, Expr *e);
 
 Call *function_call(unsigned char isMethod, char *name, int scope, Expr *elist);
 
+llist_t llist(int i);
+llist_t llist_merge(llist_t l1, llist_t l2);
+void llist_patch(llist_t list, int label);
+void patch_label(unsigned quad, unsigned label);
+
+Stmt *stmt(void);
 void print_call(Call *c);
+void print_expression(Expr *e);
 
-
+void printQuads(void);
 #endif
